73d2b77a95c5cc9a94d8c134d7d676bd1986f36a
Karen <karen.oliveira@estudante.ufjf.br>
2023-05-30 19:37:34 -0300

    adiciona estado basico

--- main.js
+++ main.js
@@ -1,7 +1,10 @@
 const form = document.forms.entrada;
 form.addEventListener('submit', envia);
 
+let estado = 0;
+
 function envia(evento) {
     evento.preventDefault();
     console.log('Formulário enviado!');
+    estado++;
 }
