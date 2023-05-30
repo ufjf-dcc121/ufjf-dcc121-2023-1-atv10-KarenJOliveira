87d06e6df01c7401fac9155ec96b6eb6e73498ce
Karen <karen.oliveira@estudante.ufjf.br>
2023-05-30 19:41:51 -0300

    atualiza estado no inicio

--- main.js
+++ main.js
@@ -2,9 +2,15 @@ const form = document.forms.entrada;
 form.addEventListener('submit', envia);
 
 let estado = 0;
-
+atualiza();
 function envia(evento) {
     evento.preventDefault();
     console.log('Formulário enviado!');
     estado++;
+    atualiza();
+}
+
+function atualiza(){
+    const ol = document.querySelector('ol');
+    ol.innerHTML=`<li>${estado}</li>`;
 }
