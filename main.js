a5e98084816ad0ac6f51c95b8f39c858e0f67657
Karen <karen.oliveira@estudante.ufjf.br>
2023-05-30 20:45:35 -0300

    botao de remover

--- main.js
+++ main.js
@@ -1,4 +1,4 @@
-import store from "./store.js";
+import {adicionar , getItens} from "./store.js";
 
 const form = document.forms.entrada;
 form.addEventListener('submit', envia);
@@ -8,7 +8,7 @@ function envia(evento) {
     evento.preventDefault();
     console.log('Formulário enviado!');
     const n = form.valor.value;
-    store.estado.push(n);
+    adicionar(n);
     form.valor.value = "";
     form.valor.focus();
     atualiza();
@@ -17,9 +17,10 @@ function envia(evento) {
 function atualiza(){
     const ol = document.querySelector('ol');
     ol.innerHTML = "";
-    for(let i=0;i<store.estado.length;i++){
+    const itens = getItens();
+    for(let i=0;i<itens.length;i++){
         const li = document.createElement('li');
-        li.textContent = store.estado[i];
+        li.textContent = itens[i];
         ol.appendChild(li);
     }
 }
